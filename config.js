b0cecf54429c698d52b54dae6cb3f85909d7b66e
Kakarot <107659390+Jeroscie@users.noreply.github.com>
2024-03-15 22:09:19 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,69 @@
+
+const config = {
+
+    "help": [
+        {
+            "title": "help",
+            "description": "for a list of commands(add flags '-d' for commands description)",
+            "info": ["help or ls command gives the list of commands","add '-d' flag for description of all commands also","write command after help to get info about that specific command like 'help typing'"]
+        },
+        {
+            "title": "clear",
+            "description": "to clear the terminal",
+            "info": ["clear or cls command clears the terminal"]
+        },
+        {
+            "title": "about",
+            "description": "to learn more about me",
+            "info": ["use about command to learn more about me"]
+        },
+        {
+            "title": "learnings",
+            "description": "to see my skills",
+            "info": ["use skills command to see my skills"]
+        }
+    ],
+    "terminal": {
+        "user": "$CD:/Maico_Larada",
+        "host": "sudo",
+        "path": "Please Type about to see my Info or type skills Instead to see my learnings from week 1 - 8"
+    },
+    "blogs": [
+        {
+            "site": "Medium",
+            "url": "https://medium.com/feed/@sidharth.sherry"
+        }
+    ],
+    "cheer": {
+        "responseArray": [
+            "Thank you! It makes my day😊😊😊",
+            "It is great to hear that way!😁😁😁",
+            "I would love to take credit😂😂😂",
+            "That's so good to hear! I'm glad😍😍😍"
+        ]
+    },
+    "about": "🎓 Im a grade 12 student in ALC and going to graduate this year!",
+    "social": [
+        {
+           
+        }
+    ],
+    "skills": [
+        {
+            "title": "My skills learnings from Week 1 to 8 Are: ",
+            "description": "Through my study of Media and Information Literacy (MIL), I've gained valuable insights into the responsible and ethical use of information in today's digital age. Media information Letiracy emphasizes various principles and practices that contribute to informed decision-making and ethical behavior in handling information. Ethical Use of Information underscores the importance of using information ethically, which includes respecting intellectual property rights. I've learned the significance of acknowledging and honoring the creators of content and avoiding any unauthorized use or reproduction of their work. Avoidance of Plagiarism I've learned about the detrimental effects of plagiarism and the importance of giving credit where it's due. Media Information Letiracy teaches the ethical imperative of properly citing sources and providing attribution to avoid plagiarism and uphold academic integrity. Responsible Sharing of Information encourages responsible sharing of information, recognizing the impact that our actions can have on others. I've learned the importance of critically evaluating information before sharing it and ensuring that it is accurate, reliable, and appropriate for the intended audience. Critical Evaluation Skills has equipped me with the skills to critically evaluate information, including assessing its credibility, accuracy, and bias. I've learned to question sources, verify information, and discern between fact and opinion, empowering me to make informed judgments. Digital Citizenship emphasizes the role of individuals as responsible digital citizens. I've learned about the ethical considerations involved in online communication, social media use, and digital content creation, fostering a sense of responsibility and accountability in the digital realm. In conclusion, my learning in Media Information Letiracy has not only deepened my understanding of how to effectively navigate and utilize information but has also instilled in me a commitment to ethical behavior, responsible sharing, and critical thinking in the digital landscape."
+        },
+        {
+            "title": "Other Skills are:",
+            "description": "Problem Solving, Team Leadership, Community Management, Team Building"
+        }
+    ],
+    "neofetch": {
+        "name": "Sidharth Sethi",
+        "title": "MERN Developer",
+        "skills": "Frontend, Backend, Cloud",
+        "shell": "zsh",
+        "languages": "Javascript, C++, HTML/CSS, SQL"
+    },
+}
+export default config
